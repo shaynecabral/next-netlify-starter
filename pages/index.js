73e1d2d19c7c6fd4524d1fe4923738f82f447328
shaynecabral <103328497+shaynecabral@users.noreply.github.com>
2022-04-13 22:16:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-var img = document.createElement ("img");
-img.src = "Logo_Dinall_Aviation.jpg";
+
 
 export default function Home() {
   return (
@@ -14,13 +13,7 @@ export default function Home() {
 
       <main>
       <Header title="COMING SOON!" />
-
-  
-      var block = document.getElementById("x");
-      div.appendChild(img)
-      
-
-        <p className="description">
+              <p className="description">
           STC MANAGEMENT FOR YOUR FLEET!
           Dinall Aviation provides document services for previously embodied STC's.
           Let us help you on your next aircraft importation! 
