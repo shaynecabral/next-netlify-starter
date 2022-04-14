772a0ce0da93bf083207a04231840eb9713886ad
shaynecabral <103328497+shaynecabral@users.noreply.github.com>
2022-04-13 22:09:10 -0400

    img_contact

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+var img = document.createElement ("img");
+img.src = "Logo_Dinall_Aviation.jpg";
 
 export default function Home() {
   return (
@@ -12,12 +14,18 @@ export default function Home() {
 
       <main>
       <Header title="COMING SOON!" />
+
+  
+      var block = document.getElementById("x");
+      div.appendChild(img)
+      
+
         <p className="description">
           STC MANAGEMENT FOR YOUR FLEET!
           Dinall Aviation provides document services for previously embodied STC's.
           Let us help you on your next aircraft importation! 
 
-          Contact: info@dinallaviation.ca
+          Contact: info@dinallaviation.ca - +1 514 793 1235
         </p>
     
       </main>
